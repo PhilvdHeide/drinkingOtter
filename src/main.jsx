9e968c486e7bf1a3a34a1d931ecfc4eea6b535e3
PhilvdHeide <phil.vdh@mail.de>
2025-01-19 01:35:51 +0100

    fix:feat: rebuilding dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Add dark class to HTML element
+document.documentElement.classList.add('dark')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
